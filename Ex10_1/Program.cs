70ed2bb90ebd255d128be1ca2f0902d7bb16cc46
Evgeniy <evg961@mail.ru>
2022-08-12 18:28:22 +0300

    Ex 10 1

---
+++ Ex10_1/Program.cs
@@ -0,0 +1,12 @@
+﻿int[] array = {4,5,6};
+int n = array.Length;
+int find = 5;
+int index = 0;
+while(index < n)
+{
+    if(array[index]==find)
+    {
+        Console.WriteLine(find);
+    }
+    index++;
+}
