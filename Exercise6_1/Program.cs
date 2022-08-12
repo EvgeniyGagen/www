9a9877dfae180d10729a437e1f10066685053ac5
Evgeniy <evg961@mail.ru>
2022-08-12 13:19:42 +0300

    Ex 6 1

---
+++ Exercise6_1/Program.cs
@@ -0,0 +1,17 @@
+﻿int a;                             
+Console.Write("введите число a: ");
+a = Convert.ToInt32(Console.ReadLine());
+ 
+if (a % 2==0)
+{
+    Console.WriteLine("число четное");
+           
+}
+else
+{
+    Console.WriteLine("Число нечетное");
+}
+ 
+
+    
+
