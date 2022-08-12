35ac741a2b7c7f366aee46162309923a85993bf4
Evgeniy <evg961@mail.ru>
2022-08-12 13:00:03 +0300

    Exercise

---
+++ Ex4_1/Program.cs
@@ -0,0 +1,17 @@
+﻿
+
+int Max(int arg1, int arg2, int arg3)
+{
+    int resalt = arg1;
+    if(arg2>resalt) resalt = arg2;
+    if(arg3>resalt) resalt = arg3;
+    return resalt;
+}
+int a = 2;
+int b = 3;
+int c = 7;
+
+int max = Max(a, b, c);
+
+Console.Write("max= ");
+Console.WriteLine(max);
