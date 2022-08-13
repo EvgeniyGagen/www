433dd9da0765a1c08a2c046f7a47ed9d01a9bbd4
Evgeniy <evg961@mail.ru>
2022-08-13 20:00:23 +0300

    Ex10_2

---
+++ Ex10_3/Program.cs
@@ -0,0 +1,14 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+       
+        int[] number = {9,1,8};
+        Console.WriteLine(number[1]); // 1
+        // 0  - 1 1 - 3
+        //int len = number.Length;
+        //Console.WriteLine(number[number.Length-1]);
+
+
+    }
+}
