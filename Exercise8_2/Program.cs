3ae82b963c3facadd067b47bddea4ea120254c00
Evgeniy <evg961@mail.ru>
2022-08-13 21:35:57 +0300

    Exercise 8 2

---
+++ Exercise8_2/Program.cs
@@ -0,0 +1,16 @@
+﻿int[] array = {1, 2, 3, 4, 5, 6, 7, 8};
+int n = array.Length;
+int index = 0;
+while(index < n)
+{
+    if(array[index] % 2==0)
+    {
+        Console.Write("четное число -");
+        Console.WriteLine(array[index]);
+    }
+    else
+    {
+        Console.WriteLine("нечетное число");
+    }
+    index++;
+}
