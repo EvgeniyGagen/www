92d3a94c17549740494ab1f43bb943dbd47c650a
Evgeniy <evg961@mail.ru>
2022-08-14 12:53:56 +0300

    Exercise 13 3

---
+++ Exercise13_3/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+class Program
+{
+    static void SelectRase()
+    {
+        string[] namesRase = {"3","2","6","7","9"};
+        for (int i = 0; i < namesRase.Length-1; i++)
+        {
+            Console.Write(i+1 + " - " + namesRase[i] + ", ");
+        }
+        Console.WriteLine(namesRase.Length + " - " + namesRase[namesRase.Length-1] + ".");
+        Console.WriteLine("Укажите вашу позицию? ");
+        int numberRase = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Ваша число: " + namesRase[numberRase-1]);
+    }
+    
+    
+    static void WriteSelect(string[] arg)
+    {
+        for (int i = 0; i < arg.Length-1; i++)
+        {
+            Console.Write(i+1 + " - " + arg[i] + ", ");
+        }
+        Console.WriteLine(arg.Length + " - " + arg[arg.Length-1] + "." );
+    }
+
+    static void Main(string[] arg)
+    {
+        SelectRase();
+        //SelectSex();
+        //SelectClass();
+    }
+
+
+}
